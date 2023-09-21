dc4f610d9d7656df0675b7e9cebf803dd4695a54
JorgeSalgado7 <j.salgado7@outlook.com>
2023-09-21 17:34:28 -0600

    Feature: New accommodations structure

--- accommodations/index.php
+++ accommodations/index.php
@@ -6,6 +6,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Accomodations | Emperor’s Pavilion</title>
 	<link rel="stylesheet" href="../css/main.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css"/>
 </head>
 
 <body>
@@ -40,53 +42,82 @@
 	</nav>
 
 	<!--HERO-->
-	<div class="estates_accommodations_hero">
-
+	<div class="estates_accommodations_hotel_hero">
 		<div class="container">
-			<h1 class="estates_accommodations_hero__h1">A PALACE OF LUXURY AND COMFORT</h1>
+			<h1 class="estates_accommodations_hotel_hero__h1">RELAX IN ULTIMATE LUXURY AND COMFORT</h1>
 		</div>
-
 	</div>
 
-	<!-- THE DIFFERENCE -->
-	<div class="estates_heritage">
+	<!-- Emperor’s Pavilion  -->
+	<div class="estates_accommodations_hotel__white">
 
+		<!-- RESPONSIVE TITLE -->
 		<div class="container">
-
-			<h2 class="estates_heritage__h2">Accommodations at Emperor’s Pavilion</h2>
-			<p class="estates_heritage__p">The Emperor’s Pavilion unit is designed to inspire awe in visitors and give them the ultimate spot to relax and indulge amid splendor. From the moment one steps into the palatial entrance vestibule, the abundant space, thoughtful layout, meticulous attention to detail, and lavish furnishings and finishes reign supreme. Enjoy two full floors of luxury showcasing the majestic bird’s eye views beyond, with bedrooms and living areas on the first level, and the most grand rooftop terrace ever conceived occupying the second level.</p>
-
+			<h2 class="estates_accommodations_hotel__h2_responsive">Emperor’s Pavilion </h2>
 		</div>
 
-	</div>
-
-	<!-- DESTINATIONS -->
-	<div class="estates_destinations">
-
 		<div class="container">
 
-			<h2 class="estates_destinations__h2">Emperor’s Pavilion ACCOMMODATIONS</h2>
+			<div class="estates_accommodations_hotel__white__text">
 
-			<div class="estates_destinations__grid">
+				<h2 class="estates_accommodations_hotel__white__text__h2">Emperor’s Pavilion</h2>
 
-				<div class="estates_destinations__grid__item"></div>
+				<ul class="estates_accommodations_hotel__white__text__ul">
+					<li>Bed: Two King-Size Beds and Four Double Beds</li>
+					<li>Bedroom Furnishings: Bedside Tables with Lamps, Dressing Tables with Chairs and Mirrors, and Armchairs with Tables</li>
+					<li>Bathrooms: Five and a Half Bathrooms with Two Jacuzzi Tubs and Double Sinks</li>
+					<li>Outdoor: Private Terraces for Each Bedroom Plus Expansive Private Rooftop Terrace with Kitchen, Bar, Half Bath, and Multiple Sitting, Lounging, and Dining Areas</li>
+					<li>Pool: Large Private Plunge Pool</li>
+					<li>Private Massage Room</li>
+					<li>Kitchen: In-Unit Kitchen and Bar with Premium Appliances and Breakfast Bar</li>
+					<li>Dining: Dining Area with Table for 10</li>
+					<li>Living Room: Two Sofas with Coffee Tables and Armchairs with Tables</li>
+					<li>TVs: Located in Bedrooms and Living Rooms</li>
+					<li>Pool: Large Private Plunge Pool</li>
+				</ul>
+			</div>
 
-				<!-- NUEVO VALLARTA -->
-				<div class="estates_destinations__grid__item">
+			<button class="estates_accommodations_hotel__white__img" id="estatesOneBedroomButton"
+				onclick="openEstatesOneBedroomModal()">
+				<img src="../img/accommodations/bedrooms/emperors-pavilion.webp" alt="">
 
-					<div class="estates_destinations__grid__item__img">
-						<img src="../img/about/nv.webp" alt="">
-					</div>
-
-					<h3 class="estates_destinations__grid__item__h3">Vidanta Nuevo Vallarta:<br>Now Open</h3>
-
-					<a href="/emperors-pavilion/accommodations/nuevo-vallarta/" class="estates_destinations__grid__item__link">Explore Emperor’s Pavilion
-						nuevo vallarta</a>
-
+				<div class="estates_accommodations_hotel__white__img__button">
+					<img src="../img/accommodations/plan.png" alt="">
+					view floorplan
 				</div>
+			</button>
 
-				<div class="estates_destinations__grid__item"></div>
+		</div>
 
+	</div>
+
+	<!-- Emperor’s Pavilion  MODAL -->
+	<div class="estates_accommodations_hotel__modal" id="estatesOneBedroomModal">
+
+		<button class="estates_accommodations_hotel__modal--close" onclick="closeEstatesOneBedroomModal()">
+			<i class="bi bi-x-lg"></i>
+		</button>
+
+		<div class="container">
+			<h1 class="estates_accommodations_hotel__modal__title">Emperor’s Pavilion </h1>
+		</div>
+
+		<div class="container">
+
+			<div class="swiper">
+
+				<div class="swiper-wrapper">
+					<div class="swiper-slide">
+					<img src="../img/accommodations/floorplans/level-1.webp" alt="">
+					</div>
+					<div class="swiper-slide">
+					<img src="../img/accommodations/floorplans/level-2.webp" alt="">
+					</div>
+				</div>
+	
+				<div class="swiper-button-prev"><i class="bi bi-arrow-left"></i></div>
+				<div class="swiper-button-next"><i class="bi bi-arrow-right"></i></div>
+	
 			</div>
 
 		</div>
@@ -101,6 +132,8 @@
 </body>
 
 <script src="../js/menu.js"></script>
-
+<script src="../js/accommodations/nv.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+<script src="../js/accommodations/bedroomsCarrousel.js"></script>
 
 </html>
