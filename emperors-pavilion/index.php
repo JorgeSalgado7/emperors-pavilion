e6022376b92f8cb4389bf1c91def5e737dbfcd4c
JorgeSalgado7 <j.salgado7@outlook.com>
2023-09-29 10:29:43 -0600

    Fix: About & Heritage

--- emperors-pavilion/index.php
+++ emperors-pavilion/index.php
@@ -74,7 +74,7 @@
 				</div>
 
 				<div class="estates_destinations__card__text">
-					<h2>Vidanta Nuevo Vallarta:<br>Now open</h2>
+					<h2>Vidanta Nuevo Vallarta:<br>Coming soon</h2>
 					<a href="/emperors-pavilion/accommodations/">Explore Emperor's Pavilion<br>Nuevo Vallarta</a>
 				</div>
 				
